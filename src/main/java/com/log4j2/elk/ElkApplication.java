adbb956447ed9a29c724f99ecb109c46c7286899
Hare <h1124128299@gmail.com>
2021-11-28 19:51:25 +0800

    elk整合log4j2

---
+++ src/main/java/com/log4j2/elk/ElkApplication.java
@@ -0,0 +1,13 @@
+package com.log4j2.elk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElkApplication.class, args);
+    }
+
+}
